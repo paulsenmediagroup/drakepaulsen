46c870f74a1628b4091d8874ea20a2383086aaed
Paulsen Media Group <38926319+paulsenmediagroup@users.noreply.github.com>
2018-12-29 06:58:37 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -27,7 +27,7 @@ $errorMessage = 'There was an error while submitting the form. Please try again 
  */
 
 // if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
-error_reporting(E_ALL & ~E_NOTICE);
+error_reporting(0);
 
 try
 {
@@ -62,7 +62,7 @@ catch (\Exception $e)
 
 
 // if requested by AJAX request return JSON response
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+if (!empty($_SERVER['HTTPS_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTPS_X_REQUESTED_WITH']) == 'xmlhttprequest') {
     $encoded = json_encode($responseArray);
 
     header('Content-Type: application/json');
