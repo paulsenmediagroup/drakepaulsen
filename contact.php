8d5209b648886eaa613ae92b82e169aa2663737b
Paulsen Media Group <38926319+paulsenmediagroup@users.noreply.github.com>
2018-12-29 07:08:08 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = '<drake@paulsenmediagroup.com>';
+$from = <drake@paulsenmediagroup.com>';
 
 // an email address that will receive the email with the output of the form
 $sendTo = '<drakepaulsen@gmail.com>';
